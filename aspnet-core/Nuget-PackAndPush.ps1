cb7e7c36cd9d6b6798389ce02d87655aadd9c9ec
陈磊 <gbdvjxu1257q@outlook.com>
2023-03-05 17:34:59 +0800

    update abp@7.0

---
+++ aspnet-core/Nuget-PackAndPush.ps1
@@ -0,0 +1,9 @@
+﻿$version = Read-Host '请输入版本号！'
+Remove-Item nupkgs/*
+dotnet pack -c Release --output nupkgs /p:Version=$version
+cd nupkgs
+dotnet nuget push -s http://121.196.122.107:8880/v3/index.json -k 3556213f-13ec-3b23-b60d-d5b3570eb4b2 *.nupkg
+
+"`n按任意键退出：" ;
+[Console]::Readkey() |　Out-Null ;
+Exit ;
--- aspnet-core/host/SuperAbp.RegionManagement.AuthServer/Migrations/20230305093201_UpdateAbp_7_0.Designer.cs
+++ aspnet-core/host/SuperAbp.RegionManagement.AuthServer/Migrations/20230305093201_UpdateAbp_7_0.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SuperAbp.RegionManagement.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace SuperAbp.RegionManagement.Migrations
 {
     [DbContext(typeof(AuthServerDbContext))]
-    partial class AuthServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230305093201_UpdateAbp_7_0")]
+    partial class UpdateAbp70
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
